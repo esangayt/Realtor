779e327ea678b4bfffa2f06b94e82d4623386f2a
Erlin Sangay <24_de_7@protonmail.com>
2023-09-19 17:41:53 -0500

    Configuración en settings.py para debugear

--- realtor/wsgi.py
+++ realtor/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realtor.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realtor.settings')
 
 application = get_wsgi_application()
