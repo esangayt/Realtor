8679a54136e52eb10b62cef9c672c29a37f14763
Erlin Sangay <24_de_7@protonmail.com>
2023-09-23 17:39:55 -0500

    Añade headers al CustomResponse

--- base/CustomResponse.py
+++ base/CustomResponse.py
@@ -7,14 +7,21 @@ from base.status_text import status_texts
 
 
 class CustomResponse(Response):
-    def __init__(self, data=None, status_code=status, message=None):
+    def __init__(self, data=None, status_code=status, message=None, headers=None):
         status_code = self.normalize_status_code(status_code)
         super().__init__(
-            self.normalize_content(data, status_code, message),
-            status_code
+            data=self.normalize_content(data, status_code, message),
+            status=status_code,
+            headers=headers
         )
 
-    def normalize_content(self, data, status_code, message):
+    def normalize_content(self, data, status_code: int, message: str):
+        """
+        :param data:
+        :param status_code:
+        :param message:
+        :return:
+        """
         body = {
             'error': not status.is_success(status_code),
             'message': message or status_texts.get(status_code),
@@ -38,20 +45,20 @@ class CustomResponse(Response):
         return cls(content, status_code, message)
 
     @classmethod
-    def item(cls, data=None, status_code=status.HTTP_200_OK, message=None):
+    def item(cls, data=None, status_code=status.HTTP_200_OK, message=None, headers=None):
         if not data and status_code == status.HTTP_200_OK:
             return cls(status_code=status.HTTP_404_NOT_FOUND)
 
-        return cls(data, status_code, message)
+        return cls(data, status_code, message, headers)
 
     @classmethod
-    def updated(cls, content, message=None):
-        return cls.item(content, status.HTTP_202_ACCEPTED, message)
+    def updated(cls, content, message=None, headers=None):
+        return cls.item(content, status.HTTP_202_ACCEPTED, message, headers)
 
     @classmethod
-    def stored(cls, content, message=None):
-        return cls.item(content, status.HTTP_201_CREATED, message)
+    def stored(cls, content, message=None, headers=None):
+        return cls.item(content, status.HTTP_201_CREATED, message, headers)
 
     @classmethod
-    def destroyed(cls, content, message):
-        return cls.updated(content, message)
+    def destroyed(cls, content, message, headers=None):
+        return cls.updated(content, message, headers)
