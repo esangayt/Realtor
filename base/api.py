d6c89ef0d33f3fefd4a518d405dc627610b38d3b
Erlin Sangay <24_de_7@protonmail.com>
2023-09-24 02:29:36 -0500

    Nuevas clases base en los Views base

--- base/api.py
+++ base/api.py
@@ -1,22 +1,36 @@
 from rest_framework import generics, viewsets
 
 
+class GeneralReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
+
+    def get_queryset(self):
+        model = self.get_serializer().Meta.model
+        return model.objects.filter()
+
+
+class GeneralRPDViewSet(generics.RetrieveUpdateDestroyAPIView):
+
+    def get_queryset(self):
+        model = self.get_serializer().Meta.model
+        return model.objects.filter()
+
+
+class GeneralCreateViewSet(generics.CreateAPIView):
+    def get_queryset(self):
+        model = self.get_serializer()
+        return model.objects.all()
+
+
 class GeneralListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
+        pk = self.kwargs.get('pk')
         model = self.get_serializer().Meta.model
-        return model.objects.filter(is_active=True)
-
-
-class GeneralAPIView(generics.GenericAPIView):
-
-    def get_queryset(self):
-        model = self.get_serializer().Meta.model
-        return model.objects.filter(is_active=True)
+        return model.objects.filter(building=pk)
 
 
 class GeneralModelViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         model = self.get_serializer().Meta.model
-        return model.objects.filter(is_active=True).order_by('id')
+        return model.objects.filter().order_by('id')
